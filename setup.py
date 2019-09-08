f5d16e26cc959e42ce70677fb146e6a50a95dd72
Steve Baer <steve@mcneel.com>
2019-09-08 10:15:59 -0700

    add requirements back to see if they work with readthedocs

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcneel/compute.rhino3d",
-    install_requires=['requests'],
+    install_requires=['requests', 'rhino3dm'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
